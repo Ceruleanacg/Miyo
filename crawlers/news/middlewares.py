add59f807512983ac15894b1c58bd6c24a89b477
Shuyu <Ceruleanacg@gmail.com>
2016-02-13 23:19:31 +0800

    Finish Captcha Module

---
+++ crawlers/news/middlewares.py
@@ -0,0 +1,13 @@
+import random
+
+
+class UserAgentMiddleware(object):
+    def __init__(self, agents):
+        self.agents = agents
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings.getlist('USER_AGENTS'))
+
+    def process_request(self, request, spider):
+        request.headers.setdefault('User-Agent', random.choice(self.agents))
--- crawlers/news/pipelines.py
+++ crawlers/news/pipelines.py
@@ -7,7 +7,10 @@
 
 from base.model import *
 
+import scrapy
+
 from scrapy.exceptions import DropItem
+from scrapy.pipelines.images import ImagesPipeline
 
 
 class NewsPipeline(object):
@@ -22,4 +25,16 @@ class NewsPipeline(object):
                 news = News(**dict(item))
                 news.save()
 
-            return item
+        return item
+
+
+class ImagePipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for image_url in item['image_urls']:
+            yield scrapy.Request(image_url)
+
+    def item_completed(self, results, item, info):
+        images = [x['path'] for ok, x in results if ok]
+        if images:
+            item['images'] = images
+        return item
