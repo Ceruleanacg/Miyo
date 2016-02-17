26bb24647ead85b067246427b03a14105c353ad3
Shuyu <Ceruleanacg@gmail.com>
2016-02-17 22:48:20 +0800

    Add unfollow

--- star/handlers.py
+++ star/handlers.py
@@ -18,9 +18,13 @@ class FollowHandler(BaseRequestHandler):
         star_id = self.get_argument('star_id')
 
         if ObjectId(star_id) in user.following_stars:
-            return self.common_response(FAILURE_CODE, "已经关注该明星!")
+            return self.common_response(FAILURE_CODE, "已经关注该明星")
 
         star = Star.objects(id=star_id).first()
+
+        if not star:
+            return self.common_response(FAILURE_CODE, "该明星不存在")
+
         star.fans.append(user.id)
         star.save()
 
@@ -28,3 +32,28 @@ class FollowHandler(BaseRequestHandler):
         user.save()
 
         return self.common_response(SUCCESS_CODE, "关注成功")
+
+    def delete(self, *args, **kwargs):
+        token = self.get_argument('token')
+
+        AccountHelper.verify_token(token)
+
+        star_id = self.get_argument('star_id')
+
+        user = AccountHelper.get_user_by_token(token)
+
+        if ObjectId(star_id) not in user.following_stars:
+            return self.common_response(FAILURE_CODE, "没有关注该明星")
+
+        star = Star.objects(id=star_id).first()
+
+        if not star:
+            return self.common_response(FAILURE_CODE, "该明星不存在")
+
+        star.fans.remove(user.id)
+        star.save()
+
+        user.following_stars.remove(star.id)
+        user.save()
+
+        return self.common_response(SUCCESS_CODE, "取消关注成功")
